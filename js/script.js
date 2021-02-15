a39c8d2ef17dc986a8fecaf94c1a5f6ae6505605
= <=>
2021-02-15 15:52:41 +0700

    Update Teks dan Gambar Faq

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// $("#btn_pilihpaket").on('click', function (event) {
+// 	// Make sure this.hash has a value before overriding default behavior
+// 	if (this.hash !== "") {
+// 		// Prevent default anchor click behavior
+// 		event.preventDefault();
+// 		// Store hash
+// 		let hash = this.hash;
+// 		// Using jQuery's animate() method to add smooth page scroll
+// 		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+// 		$('html, body').animate({
+// 			scrollTop: $(hash).offset().top - 50
+// 		}, 1000, 'easeOutSine')
+// 	} // End if
+// });
+
+let coll = document.getElementsByClassName("faqjudul");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		let content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+	});
+}
